db9b095daa159ef551290208b6bdf80d6d20c4a4
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-29 23:35:47 -0500

    creation of the Category model and Product model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
-    nombre = models.CharField(max_length=100)
+    nme = models.CharField(max_length=100)
+
+
+class Producto(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
