17ee13912ffb572a42f7c56e0395a322fdb36aba
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-29 23:41:43 -0500

    creation of the CartItem

--- ecommerce/cart/models.py
+++ ecommerce/cart/models.py
@@ -5,4 +5,10 @@ from django.contrib.auth.models import User
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, through="ItemCarrito")
+    products = models.ManyToManyField(Product, through="CartItem")
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
