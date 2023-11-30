567f602e34d2933dad50f013bf1dc4c7b86692b1
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-29 23:37:15 -0500

    creation of the Category model and Product model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     nme = models.CharField(max_length=100)
 
 
-class Producto(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
