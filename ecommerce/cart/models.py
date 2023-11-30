43746a2e06341a4091a36194fd94cb7beb95e6b1
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-29 23:40:22 -0500

    creation of the Cart model

--- ecommerce/cart/models.py
+++ ecommerce/cart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through="ItemCarrito")
