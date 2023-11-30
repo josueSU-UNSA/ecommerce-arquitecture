fe7e9a6f10f3c5647df8eb3c04a52b7bb682caff
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-30 06:23:48 -0500

    Added a post detail view

--- ecommerce/blog/views.py
+++ ecommerce/blog/views.py
@@ -5,3 +5,8 @@ from blog.models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_detail(request, publicacion_id):
+    post = Post.objects.get(pk=publicacion_id)
+    return render(request, "blog/post_detail.html", {"post": post})
