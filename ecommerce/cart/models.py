c5dd62a852dce2ff82d798a0a58aa0415d938fc7
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-29 23:43:43 -0500

    creation of the Order model

--- ecommerce/cart/models.py
+++ ecommerce/cart/models.py
@@ -12,3 +12,10 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through="ItemOrder")
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
