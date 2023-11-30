00061606cf2e17e8ef557f730b10f380349e713f
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-30 00:58:06 -0500

    Created a view to send message

--- ecommerce/contact/views.py
+++ ecommerce/contact/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from contact.models import ContactMessage
 
-# Create your views here.
+
+def send_message(request):
+    if request.method == "POST":
+        form = ContactMessage(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("mensaje_enviado")
+    else:
+        form = ContactMessage()
+
+    return render(request, "contact/send_message.html", {"form": form})
