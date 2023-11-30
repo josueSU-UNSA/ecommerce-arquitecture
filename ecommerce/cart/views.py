4448c9afc9c75c0c060ab020f27433f14c527a72
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-30 00:37:39 -0500

    creating vue to add an item to the cart

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from cart.models import Cart, CartItem, Order
 from products.models import Product
 
@@ -8,3 +8,18 @@ def show_cart(request):
     items = CartItem.objects.filter(cart=cart)
     context = {"carrito": cart, "items": items}
     return render(request, "cart/show_cart.html", context)
+
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    cart = get_object_or_404(Cart, user=request.user)
+
+    item, created = CartItem.objects.get_or_create(
+        product=product, cart=cart, defaults={"quantity": 1}
+    )
+
+    if not created:
+        item.quantity += 1
+        item.save()
+
+    return redirect("show_cart")
