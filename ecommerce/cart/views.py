9f928c5abcf2cfcbbd5a19cebe65ac6ad654bde4
JOSUE_SU <jsumare@unsa.edu.pe>
2023-11-30 00:28:50 -0500

    creating vue to specific a cart

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from cart.models import Cart, CartItem, Order
+from products.models import Product
 
-# Create your views here.
+
+def show_cart(request):
+    cart = get_object_or_404(Cart, user=request.user)
+    items = CartItem.objects.filter(cart=cart)
+    context = {"carrito": cart, "items": items}
+    return render(request, "cart/show_cart.html", context)
